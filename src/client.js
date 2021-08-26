89aa65a88a1a1b41ee9b645fa671c8fc4f7db679
Emmely Lundberg <emmely.lundberg@noaignite.com>
2021-08-26 10:19:30 +0200

    Portfolio init.

---
+++ src/client.js
@@ -0,0 +1,8 @@
+import sanityClient from "@sanity/client"
+import React from 'react';
+
+// projectId found in sanity.json
+export default function sanityClient({projectId: "dkxauvcq", dataset: "production"}) {
+ return (<div></div>)
+}
+
